4799d1774b9378909fa2d61088c3a29fdf03eb2c
suraj-singh12 <surajsingh5092@gmail.com>
2024-04-26 10:11:13 +0530

    game using beans without separate gamingConfig

--- src/main/java/com/learning/springboot/App03GamingSpringBeans.java
+++ src/main/java/com/learning/springboot/App03GamingSpringBeans.java
@@ -1,13 +1,25 @@
 package com.learning.springboot;
 import com.learning.springboot.game.GameRunner;
 import com.learning.springboot.game.GamingConsole;
+import com.learning.springboot.game.MarioGame;
 import com.learning.springboot.game.SuperContraGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class App03GamingSpringBeans {
+    @Bean
+    public GamingConsole game() {
+        return new MarioGame();
+    }
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
+    }
     public static void main(String[] args) {
 
-        try(var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+        try(var context = new AnnotationConfigApplicationContext(App03GamingSpringBeans.class)) {
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
