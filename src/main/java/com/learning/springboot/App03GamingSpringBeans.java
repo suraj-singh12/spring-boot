ce5554345a698b4aff9db56e0b939d493172643f
suraj-singh12 <surajsingh5092@gmail.com>
2024-04-26 10:38:56 +0530

    using @Component, and @ComponentScan to autowire things

--- src/main/java/com/learning/springboot/App03GamingSpringBeans.java
+++ src/main/java/com/learning/springboot/App03GamingSpringBeans.java
@@ -3,16 +3,15 @@ import com.learning.springboot.game.GameRunner;
 import com.learning.springboot.game.GamingConsole;
 import com.learning.springboot.game.MarioGame;
 import com.learning.springboot.game.SuperContraGame;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.learning.springboot.game")      // now spring will be able to search for components in this package.
 public class App03GamingSpringBeans {
-    @Bean
-    public GamingConsole game() {
-        return new MarioGame();
-    }
     @Bean
     public GameRunner gameRunner(GamingConsole game) {
         return new GameRunner(game);
--- src/main/java/com/learning/springboot/game/PacmanGame.java
+++ src/main/java/com/learning/springboot/game/PacmanGame.java
@@ -1,5 +1,8 @@
 package com.learning.springboot.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PacmanGame implements GamingConsole{
     public void up() {
         System.out.println("up");
