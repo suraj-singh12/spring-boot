a6b2591ff57a3ddc0241b11ed41d186ec667ff6c
suraj-singh12 <surajsingh5092@gmail.com>
2024-04-28 09:43:51 +0530

    Bean scops (Default: Singleton), (Prototype)

---
+++ src/main/java/com/learning/springboot/examples/e1/BeanScopesLauncherApplication.java
@@ -0,0 +1,43 @@
+package com.learning.springboot.examples.e1;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class NormalClass {
+
+}
+
+@Component
+@Scope(value= ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+class PrototypeClass {
+
+}
+
+
+@Configuration
+@ComponentScan // now spring will do component scan in current file's package by default (com.learning.springboot.examples.a1)
+public class BeanScopesLauncherApplication {
+    public static void main(String[] args) {
+
+        try(var context =
+                    new AnnotationConfigApplicationContext(
+                            BeanScopesLauncherApplication.class)) {
+            System.out.println(context.getBean(NormalClass.class));
+            System.out.println(context.getBean(NormalClass.class));
+            System.out.println(context.getBean(NormalClass.class));
+            System.out.println(context.getBean(NormalClass.class));
+
+            System.out.println(context.getBean(PrototypeClass.class));
+            System.out.println(context.getBean(PrototypeClass.class));
+            System.out.println(context.getBean(PrototypeClass.class));
+            System.out.println(context.getBean(PrototypeClass.class));
+
+        }
+    }
+}
